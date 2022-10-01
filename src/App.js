47e296b694e40f8491836484f6d487fc2624a734
aligbakhtiyar <ahmadbakhtiyar@gmail.com>
2022-10-01 21:43:19 +0530

    Final

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import{BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Header from './components/Header/Header';
 import Home from './pages/Home';
 import MovieList from './components/movieList/movieList';
-
+import Movie from './pages/movie';
 
 
 
@@ -15,7 +15,7 @@ function App() {
       <Header />
         <Routes> 
            <Route index element={<Home />}></Route>
-           <Route path="movie/:id" element={<h2>moviemmmmmm</h2>}></Route>
+           <Route path="movie/:id" element={<Movie />}></Route>
            <Route path="movies/:type" element={<MovieList />}></Route>
            <Route path="/*" element={<h1>Error Page</h1>}></Route>
         </Routes>
